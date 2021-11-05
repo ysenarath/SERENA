0a62fb187144985882657e8f3effdf956be3062e
Yasas <ysenarath.93@gmail.com>
2021-11-05 00:14:31 -0400

    Fix rendering problems

--- serena/app.py
+++ serena/app.py
@@ -14,27 +14,24 @@ app = Flask(__name__, template_folder=TEMPLATE_FOLDER, static_folder=STATIC_FOLD
 app.secret_key = SECRET_KEY
 
 
-def render_index(input=None):
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    history = []
     # load status {
     if 'status' in session:
         status = session['status']
     else:
         status = {'__meta__': {'history': []}}
-    # } process input and get output {
-    output, status = chat.process_input(input, status)
-    # } update values {
-    history = status['__meta__']['history']
-    history.append({'input': input, 'output': output})
-    status['__meta__']['history'] = history
-    # } update session values {
-    session['status'] = status
     # }
-    return render_template('index.html', title='SERENA', history=history)
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    input = None
+    history = status['__meta__']['history']
     if request.method == 'POST':
         input = request.form.get('input', None)
-    return render_index(input=input)
+        # process input and get output {
+        output, status = chat.process_input(input, status)
+        # } update values {
+        history.append({'input': input, 'output': output})
+        status['__meta__']['history'] = history
+        # } update session values {
+        session['status'] = status
+        # }
+    return render_template('index.html', title='SERENA', history=history)
