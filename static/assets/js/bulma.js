24f59832cea0d1161e60a2dbf54986ae81427449
Yasas <ysenarath.93@gmail.com>
2021-11-20 11:12:41 -0500

    Add help modal

--- static/assets/js/bulma.js
+++ static/assets/js/bulma.js
@@ -16,3 +16,23 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+document.querySelector('a#open-modal').addEventListener('click', function (event) {
+    event.preventDefault();
+    let modal = document.querySelector('.modal');  // assuming you have only 1
+    let html = document.querySelector('html');
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+
+    modal.querySelector('.modal-background').addEventListener('click', function (e) {
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
+
+    modal.querySelector('.modal-close').addEventListener('click', function (e) {
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
+});
